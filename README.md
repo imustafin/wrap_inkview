c4579b4a70351e87294725b27a162c12afd9407b
Ilgiz Mustafin <ilgimustafin@gmail.com>
2022-05-16 18:41:57 +0300

    Link libinkview.so, update path configs

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ Based on generated wrappers by WrapC.
 
 For usage with PocketBook SDK specs https://github.com/imustafin/estudio-cross-configs
 
+`$SDK_SYSROOT` env var should be set to the `sysroot` dir of the SDK e.g.
+`SDK_6.3.0-5.19/SDK-A13/usr/arm-obreey-linux-gnueabi/sysroot`.
+
+
 ## Licensing
 Generated code is licensed under Eiffel Forum License 2.
--- gen.sh
+++ gen.sh
@@ -1,8 +1,7 @@
 set -e
 
-sdk=../SDK_6.3.0-5.19/SDK-A13
-sysroot=$sdk/usr/arm-obreey-linux-gnueabi/sysroot
+# in c_compile_options: literal $SDK_SYSROOT will be in Makefile.SH
 ../WrapC/bin/wrap_c \
-    --c_compile_options="-I{$sysroot,=}/usr/include/freetype2/" \
-    --full-header=$sysroot/usr/local/include/inkview.h \
+    --c_compile_options="-I\$SDK_SYSROOT/usr/include/freetype2/" \
+    --full-header=$SDK_SYSROOT/usr/local/include/inkview.h \
     --config=config.xml
